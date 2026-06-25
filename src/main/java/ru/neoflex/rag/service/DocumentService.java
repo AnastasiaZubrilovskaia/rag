666a1da88acf91474261bb32e51fe4f857c4448e
Anastasia <168160374+AnastasiaZubrilovskaia@users.noreply.github.com>
2026-06-25 20:23:07 +0400

    Add Qdrant and embedding integration

--- src/main/java/ru/neoflex/rag/service/DocumentService.java
+++ src/main/java/ru/neoflex/rag/service/DocumentService.java
@@ -1,21 +1,36 @@
 package ru.neoflex.rag.service;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.ai.document.Document;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 import ru.neoflex.rag.model.entity.DocumentInfo;
 import ru.neoflex.rag.model.entity.DocumentStatus;
 import ru.neoflex.rag.model.response.DocumentResponse;
+import ru.neoflex.rag.parser.DocumentParser;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.UUID;
 import java.util.concurrent.ConcurrentHashMap;
 
 @Service
+@RequiredArgsConstructor
 public class DocumentService {
+    private final List<DocumentParser> parsers;
+    private final ChunkingService chunkingService;
+    private final VectorStoreService vectorStoreService;
     private final Map<UUID, DocumentInfo> documents = new ConcurrentHashMap<>();
 
     public DocumentResponse upload(MultipartFile file){
+        DocumentParser parser = getParser(file.getOriginalFilename());
+        String text = parser.parse(file);
+        List<String> chunks = chunkingService.chunk(text);
+        List<Document> vectorDocuments = createDocuments(chunks, file.getOriginalFilename());
+
+        vectorStoreService.saveDocuments(vectorDocuments);
+
         UUID documentId = UUID.randomUUID();
 
         DocumentInfo documentInfo = DocumentInfo.builder()
@@ -40,6 +55,32 @@ public class DocumentService {
     public void deleteDocument(UUID id) {
         documents.remove(id);
     }
+
+    private DocumentParser getParser(String fileName) {
+
+        return parsers.stream()
+                .filter(parser -> parser.supports(fileName))
+                .findFirst()
+                .orElseThrow(
+                        () -> new IllegalArgumentException(
+                                "Unsupported file type"
+                        )
+                );
+    }
+
+    private List<Document> createDocuments(List<String> chunks, String fileName) {
+        List<Document> documents = new ArrayList<>();
+
+        for (int i = 0; i < chunks.size(); i++) {
+            Map<String, Object> metadata = Map.of("source", fileName, "chunkIndex", i);
+
+            documents.add(new Document(chunks.get(i), metadata)
+            );
+        }
+
+        return documents;
+    }
+
 
     private DocumentResponse mapToResponse(DocumentInfo documentInfo) {
 
